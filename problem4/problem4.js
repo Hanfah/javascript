f79b157f40c12efa62a32e1ca90512b856104a46
Hanfah <hanfah@mail.com>
2021-03-21 08:54:39 -0900

    first commit

--- problem4/problem4.js
+++ problem4/problem4.js
@@ -1,7 +1,7 @@
 //write a javascript function that checks whether a passed string is 
 // palindrome or not? 
 
-    function palindrome (word) {
+    function Ispalindrome (word) {
 	let len = word.length;
 	let start = word.substring(0, Math.floor(len / 2)).toLowerCase();
 	let end = word.substring(len - Math.floor(len / 2)).toLowerCase();
@@ -10,6 +10,6 @@
 	return (start === flip);
 }
 
-console.log(palindrome('radar'));
-console.log(palindrome('redder'));
-console.log(palindrome('window'));
+console.log(Ispalindrome('radar'));
+console.log(Ispalindrome('redder'));
+console.log(Ispalindrome('window'));
