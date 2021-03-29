28397e1f6ceec540c41b93527aa457c8b1651ca1
Hanfah <hanfah@mail.com>
2021-03-29 02:44:59 -0900

    first commit

---
+++ isArray/isArray.js
@@ -0,0 +1,13 @@
+var myArray = ["aye", "bee", "see", "dee"];
+
+function isitArray(myArray){
+	console.log(Array.isArray(myArray));
+	if(myArray === Array.isArray(myArray)) {
+		return true;
+
+		}
+	}
+
+
+isitArray();
+
