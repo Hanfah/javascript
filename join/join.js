e4fd31b006a4e601c2ad687f9b5fc7ba43b321f4
Hanfah <hanfah@mail.com>
2021-04-05 04:45:52 -0900

    first commit

---
+++ join/join.js
@@ -0,0 +1,16 @@
+//write a javascript program to join all
+// elements of the following array into a string
+
+//create a function
+function myFunction(join){
+	// create a variable of an array
+	var fruites = ["bana", "young", "tired", "tired"];
+	//use the join method to join the array like a string
+   var joined = fruites.join();
+   //use the console.log so we can see this in the outcome printed in the
+   //console
+   console.log(joined);
+
+}
+//execute the function
+console.log(myFunction());
