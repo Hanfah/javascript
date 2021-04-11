2c11ffb04aab201f8c0c3364e5fe4bc8e7999dd1
Hanfah <hanfah@mail.com>
2021-04-11 00:55:17 -0900

    First Commit

---
+++ sort/sort.js
@@ -0,0 +1,19 @@
+//write a javascript function to sort the items of an array
+
+//step 1. define an array of items
+//step 2. create a function 
+//step 2. inside the function return the sort method to sort oout the fruits 
+//alphabetically
+//execute the function inside a document.write, in order for anyone to see the printed 
+//results 
+
+var myFruits = ["oranges", "peach", "pinaple", "Apples", "Plum"];
+
+
+function sortFruits (array){
+	return myFruits.sort();
+	
+
+}
+
+document.write(sortFruits(myFruits));
