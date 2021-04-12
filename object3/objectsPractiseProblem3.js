cba150b589b0758058390117b7d13774cd3f7337
Hanfah <hanfah@mail.com>
2021-04-12 12:39:15 -0900

    first commit

---
+++ object3/objectsPractiseProblem3.js
@@ -0,0 +1,18 @@
+//create an object with three items init, then write a javascript
+//function that tells you the length (tells you how many items are in it)
+//of the object
+
+//step 1. define an object with  items in t
+//step 2. white a function that takes in an oject as an argument
+//step 3. use the length method to find out how
+//many objects are in the item
+//step 4. execute the function in a console.log in order
+//for the coder to see the printed results
+
+var shopping = ("jog", "book", "iron");
+
+function lengthOfList(object){
+	return shopping.length;
+}
+
+console.log(lengthOfList());
