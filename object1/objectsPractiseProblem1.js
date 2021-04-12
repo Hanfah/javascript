978a8dbf331f951afdeb3059083fa1b1a2966c8d
Hanfah <hanfah@mail.com>
2021-04-12 12:34:02 -0900

    first comit

---
+++ object1/objectsPractiseProblem1.js
@@ -0,0 +1,22 @@
+//write a javascript program to delete a property from an object
+
+//step 1. create an object of names
+//step 2. declare a function
+//step 3. within the function use the delete method to delete the property
+//step 4. return students
+//step 4. Execute the function with a console.log,
+
+var Students = {
+
+    name: "Tim",
+	Course: "javascript",
+	Level: "Master"
+
+};
+
+function deleteAProperty(obj){
+	delete Students["Level"];
+	return Students;
+
+}
+console.log(deleteAProperty(Students));
