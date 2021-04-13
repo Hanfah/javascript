994d47ef8b68dc1a15e7680110b2504448ba7986
Hanfah <hanfah@mail.com>
2021-04-13 07:39:19 -0900

    updated

--- duplicates/duplicates.js
+++ duplicates/duplicates.js
@@ -1,17 +1,39 @@
 //write a javascript program to remove duplicate items from an array
 //for this example i will be using numbers
 
-//step 1. create an array of numbers
-//step 2. create a variable to store uniquque values from the 
-//array of numbers
-//step 3. define a function that returns the unique values
-//step 4. execute the function
-
-var numbers = [7, 8, 8, 8, 8, 8, 9];
-var uniqueNumbers = new Set(numbers);
+/*define a function that takes in an array
+-define a variable to store our array of unique values, we set the default to an empty array
+-loop through the array passed into the function 
+and check if the item in our loop is currently in our array of unique values variable
+-if it's not, let's put it there (that means it's unique since 
+we'll only put it there if it's not already there)
+-when the function finishes running, returning the array of unique values*/
 
 function removeDuplicates(array){
-	return uniqueNumbers;
+	var a = [1, 2, 3, 5, 3, 3];
+	var b = [];
+	var len = a.length;
+
+	for (var i = 0; i < len; i++){
+		if(b.indexOf(a[i]) === -1){
+			b.push(a[i]);
+		}
+	}
+
+	return b;
+
+
 }
 
-console.log(removeDuplicates(uniqueNumbers));
+console.log(removeDuplicates());
+
+/*let a = [1, 8, 8, 8, 9, 6];
+obj = {};
+
+for(let i of a){
+	obj[i] = true;
+}*/
+
+
+
+//console.log(obj);
