630caa2d8bd12f974453600cac1395437a6cf369
Hanfah <hanfah@mail.com>
2021-04-14 11:17:19 -0900

    Code updated

--- sort/sort.js
+++ sort/sort.js
@@ -1,19 +1,26 @@
 //write a javascript function to sort the items of an array
 
-//step 1. define an array of items
-//step 2. create a function 
-//step 2. inside the function return the sort method to sort oout the fruits 
-//alphabetically
-//execute the function inside a document.write, in order for anyone to see the printed 
-//results 
-
-var myFruits = ["oranges", "peach", "pinaple", "Apples", "Plum"];
-
-
-function sortFruits (array){
-	myFruits.sort();
-	return myFruits;
-
+//we have to pass information to our function,
+//and that information is saved in the parameter/named variable "array"
+/*function sortFruits (array){
+    //we will now sort the array that was passed in to our function
+return array.sort(function(a, b){
+      //sort method is case sensitive, so we're comparing everything
+      //using the same case, even tho the user can pass in fruits
+      //that are upperCased
+      return a.toLowerCase() - b.toLowerCase()
+    });
 }
+//example
+var myFruits = ["oranges", "peach", "pinaple", "Apples", "Plum"];
+//when we call the function, we have to actually pass information to it
+console.log(sortFruits(myFruits))*/
+var myFruits = ["Oranges", "Peach", "Pinapple", "Apple", "Plum"];
+function sortFruits (array){
+	return array.sort(function(a, b){
+		return a.toLowerCase() - b.toLowerCase()
+	});
 
-console.log(sortFruits());
+	}
+
+console.log(sortFruits(myFruits));
