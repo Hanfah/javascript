0b5daff509cb414566d0ab425797d4ca66dced3d
Hanfah <hanfah@mail.com>
2021-04-26 04:28:42 -0900

    first commit

--- object3/objectsPractiseProblem3.js
+++ object3/objectsPractiseProblem3.js
@@ -12,8 +12,9 @@
 var phone = {model: "motorola", color: "red", camera: "50mp"};
 
 function howManyinLength (phone_specs){
+	var phone_specs = phone;
 
-return Object.keys(phone).length;	
+return Object.keys(phone_specs).length;	
 
 
 }
