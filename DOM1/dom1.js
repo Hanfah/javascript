68dd1388163d226604db99f914daa1553f0cb8bd
Hanfah <hanfah@mail.com>
2021-05-05 12:50:49 -0900

    first commit

---
+++ DOM1/dom1.js
@@ -0,0 +1,15 @@
+//Then, write a JavaScript program to set the background color of that paragraph.
+
+//write a paragragh and give it an id
+//add an eventListener with a function
+//when a visitor clicks on the paragraph it will play a function
+// declare a function
+//set a background color to the paragraph id
+addEventListener('click', onClick);
+
+function onClick(){
+	life.style.background = "red";
+	you.style.background = "pink";
+	precious.style.background = "purple";
+
+}
