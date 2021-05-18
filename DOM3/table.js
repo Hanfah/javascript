51f7ef111c135d5a9ab07197bd16b6ea3d9dc432
Hanfah <hanfah@mail.com>
2021-05-18 12:28:28 -0900

    First Commit

---
+++ DOM3/table.js
@@ -0,0 +1,36 @@
+var btn = document.getElementById("Enter");
+btn.addEventListener("click", addDataToTable);
+
+var row = 1;
+
+function addDataToTable(){
+	//declare all the element input values that will be involved in the table
+	var name = document.getElementById("name").value;
+	var age = document.getElementById("age").value;
+	var salary = document.getElementById("salary").value;
+	var city = document.getElementById("city").value;
+	var bedrooms = document.getElementById("bedrooms").value;
+
+	//declare a variable to hold the table element
+	var table = document.getElementById("table");
+
+	//add rows to the table
+	var addRow = table.insertRow(row);
+
+	//add a cell to the row
+	//add a cell to position 0, 1, 2, 3, and 4
+	var cell1 = addRow.insertCell(0);
+	var cell2 = addRow.insertCell(1);
+	var cell3 = addRow.insertCell(2);
+	var cell4 = addRow.insertCell(3);
+	var cell5 = addRow.insertCell(4);
+
+	//Add the users input as a html inside of each cell 
+	cell1.innerHTML = name;
+	cell2.innerHTML = age;
+	cell3.innerHTML = salary;
+	cell4.innerCell = city;
+	cell5.innerHTML = bedrooms;
+    //add rows to the table
+	row ++;
+}
