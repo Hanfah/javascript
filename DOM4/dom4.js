5705764cf3e5cbee54025b50ba63626cae705bc1
Hanfah <hanfah@mail.com>
2021-06-07 03:03:51 -0900

    first commit

---
+++ DOM4/dom4.js
@@ -0,0 +1,18 @@
+//create a variable to get the element
+//create a functiion that will return the length of fruits in the list
+//display the length of fruits in the documnet
+
+var countFruits = document.getElementById("fruits");
+
+function countNoOfFruits(){
+
+	 return countFruits.length;
+}
+
+document.write(countNoOfFruits());
+
+
+
+
+
+
