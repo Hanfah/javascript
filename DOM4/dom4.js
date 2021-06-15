9ac890efb62dde4c30989aa780144264109144b2
Hanfah <77494717+Hanfah@users.noreply.github.com>
2021-06-15 13:54:38 -0900

    Add files via upload
    Updated Code

--- DOM4/dom4.js
+++ DOM4/dom4.js
@@ -2,14 +2,32 @@
 //create a functiion that will return the length of fruits in the list
 //display the length of fruits in the documnet
 
-var countFruits = document.getElementById("fruits");
 
-function countNoOfFruits(){
 
-	 return countFruits.length;
+function countNoOfFruits(dropDown){
+	//select the options
+	let arrayOfOptions = dropDown.options
+	//get the number (length) of fruits recorded on the document
+	document.write(arrayOfOptions.length)
+//create a loop to loop through the options
+//i = 0, if i is less then 0 (which it is), keep counting until we have all the names of fruits listed found
+	for(let i = 0; i < arrayOfOptions.length; i++){
+		//write all the fruits listed in the document
+		document.write(arrayOfOptions[i].value);
+
+
+
+
+	}
+
+
 }
 
-document.write(countNoOfFruits());
+
+//create a variable which stores the fruits.
+var countFruits = document.getElementById("fruits");
+//run the function
+countNoOfFruits(countFruits);
 
 
 
