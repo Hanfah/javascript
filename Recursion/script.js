d34f28deee6d8e93937f7243333e38f75f9b602b
Hanfah <77494717+Hanfah@users.noreply.github.com>
2021-06-19 20:50:48 +0100

    Add files via upload

--- Recursion/script.js
+++ Recursion/script.js
@@ -1,23 +1,25 @@
-//grab the form and the button
-var frm = document.getElementById("form");
+// Write a JavaScript program to calculate the factorial of a number
 
-var btn = document.getElementById("btn");
-
+//write a function using num as an argument with the function keyword
 
 
-//addevent listener
+function factorial(num){
+	//create a variable and set it to 1 because we will start counting from 1
+	var result = 1;
 
-btn.addEventListener("click", findIt);
+	//create a for loop starting at 1. 
+	// if i is less then or equal to 1 then do a result * 1 until i has reached up to the 
+	//given number which is 5
+	for (var i = 1; i<=num; i++){
 
-function findIt(){
-	var num = document.getElementById("number").value;
-	var result = document.getElementById("result");
-	var i, mult = 1;
-	for (i = 1; i<=num; i++){
-		mult = mult*i;
+		// calculate 1 * i
+		result = result * i;
 	}
-
-	result.innerHTML = mult;
-	return false;
+//return the result
+	return result;
 }
-	
+
+//run / execute  the function to find out the factorial of 5 in the document
+document.write(factorial(5));
+
+
