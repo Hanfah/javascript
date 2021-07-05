60838db5dc45383db05e69656a632739939e823f
Hanfah <77494717+Hanfah@users.noreply.github.com>
2021-07-05 22:26:26 +0100

    Add files via upload
    first commit

---
+++ Array Problem2/script.js
@@ -0,0 +1,19 @@
+//write a simple javascript program to join all elements of the following array into a string
+
+//create an array of the colors 
+var myColor = ["red","yellow", "pink", "orange"];
+//create a function that takes in an array as an argument
+function joinALL(array){
+
+//convert the array into a string
+	myColor.toString("");
+	//join the string
+	myColor.concat();
+	
+	//return the variable
+	return myColor;
+
+};
+//execute the function and print out in the document
+document.write(joinALL());
+
